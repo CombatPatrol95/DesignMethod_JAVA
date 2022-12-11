b3e295909cef8231851a03dafd5cba193725070f
Yajusenpai <1051148569@qq.com>
2022-12-10 19:41:35 -0500

    strategy method

--- Strategy/src/ProbStrategy.java
+++ Strategy/src/ProbStrategy.java
@@ -15,12 +15,34 @@ public class ProbStrategy {
 		random = new Random(seed);
 	}
 	public Hand nextHand() {
-		
+		int bet = random.nextInt(getSum(currentHandValue));
+		int handvalue = 0;
+		if(bet < history[currentHandValue][0]) {
+			handvalue = 0;
+		}
+		else if(bet < history[currentHandValue][0] + history[currentHandValue][0]) {
+			handvalue = 1;
+		}
+		else {
+			handvalue = 2;
+		}
+		prevHandValue = currentHandValue;
+		currentHandValue = handvalue;
+		return Hand.getHand(handvalue);
 	}
 	private int getSum(int hv) {
-		
+		int sum = 0;
+		for(int i = 0; i < 3; i++) {
+			sum += history[hv][i];
+		}
+		return sum;
 	}
 	public void study(boolean win) {
-		
+		if(win) {
+			history[prevHandValue][currentHandValue]++;
+		}else {
+			history[prevHandValue][(currentHandValue + 1) % 3]++;
+			history[prevHandValue][(currentHandValue + 2) % 3]++;
+		}
 	}
 }
